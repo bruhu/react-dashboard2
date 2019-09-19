4d706566ccbcd483fb040b6f4b85b759be0a6acb
Bru <annabru.abh@gmail.com>
2019-09-19 14:42:41 +0200

    App.js properly renders MemoList.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MemoList from './components/MemoList'
 
 import './App.css';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <p> Your app will go here </p>
-
+      <MemoList></MemoList>
     </div>
   );
 }
---
+++ src/components/MemoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class MemoList extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>Finding Memo</p>
+      </React.Fragment>
+    )
+  }
+}
+
+export default MemoList
